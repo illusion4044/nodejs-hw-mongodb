fdbc15b390be27ef992715b5e1dd6f5cfd4d8ad1
Andrii <bugaychuk9aa@gmail.com>
2024-09-22 20:52:42 +0300

    fix auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -9,9 +9,9 @@ import {userSignupSchema, userSigninSchema} from "../validation/users.js";
 
 const authRouter = Router();
 
-authRouter.post("/register", validateBody(userSignupSchema), ctrlWrapper(authControllers.signupController));
+authRouter.post("/register", validateBody(userSignupSchema), ctrlWrapper(authControllers.registerController));
 
-authRouter.post("/login", validateBody(userSigninSchema), ctrlWrapper(authControllers.signinController));
+authRouter.post("/login", validateBody(userSigninSchema), ctrlWrapper(authControllers.loginController));
 
 authRouter.post("/refresh", ctrlWrapper(authControllers.refreshController));
 
