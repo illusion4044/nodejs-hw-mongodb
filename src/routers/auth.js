731b37621e2b8dfd05920a783e77bf4b35f0336f
Andrii <bugaychuk9aa@gmail.com>
2024-09-30 11:57:03 +0300

    auth and users.js updated

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,9 +1,9 @@
 import {Router} from "express";
-
 import * as authControllers from "../controllers/auth.js";
-
 import ctrlWrapper from "../utils/ctrlWrapper.js";
 import validateBody from "../utils/validateBody.js";
+import { resetPasswordSchema } from '../validation/users.js';
+import { resetPasswordController } from '../controllers/auth.js';
 
 import {userSignupSchema, userSigninSchema} from "../validation/users.js";
 
@@ -18,14 +18,11 @@ authRouter.post("/login", validateBody(userSigninSchema), ctrlWrapper(authContro
 
 authRouter.post("/refresh", ctrlWrapper(authControllers.refreshController));
 
-authRouter.post("/signout", ctrlWrapper(authControllers.signoutController));
-
-
-authRouter.post("/login", validateBody(userSigninSchema), ctrlWrapper(authControllers.loginController));
-
-authRouter.post("/refresh", ctrlWrapper(authControllers.refreshController));
-
 authRouter.post("/logout", ctrlWrapper(authControllers.logoutController));
+
+authRouter.post("/request-reset-email", validateBody(requestResetEmailSchema), ctrlWrapper(authControllers.requestResetEmailController));
+
+authRouter.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 
 
 export default authRouter;
