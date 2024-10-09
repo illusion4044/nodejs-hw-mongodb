ea0482423f12f2e6b778b169c995aad47903296a
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-10-09 16:22:07 +0300

    gen update

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,3 +14,11 @@ export const TEMPLATES_DIR = path.resolve("src", "templates");
 export const TEMP_UPLOAD_DIR = path.resolve("temp");
 
 export const UPLOAD_DIR = path.resolve("uploads");
+
+export const CLOUDINARY = {
+	CLOUD_NAME: 'CLOUD_NAME',
+	API_KEY: 'API_KEY',
+	API_SECRET: 'API_SECRET',
+};
+
+export const SWAGGER_PATH = path.resolve('docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import { env } from "./utils/env.js";
-
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
 // import logger from "../src/middlewares/loger.js";
-
 import authRouter from "./routers/auth.js";
 import contactsRouter from "./routers/contacts.js";
+import { UPLOAD_DIR } from './src/constants/index.js';
+import { swaggerDocs } from './src/middlewares/swaggerDocs.js'; // Updated import
 
 export const startServer = ()=> {
     const app = express();
@@ -19,12 +18,16 @@ export const startServer = ()=> {
     app.use(express.json());
     app.use(cookieParser());
 
+    app.use('/api-docs', swaggerDocs); // Updated to use swaggerMiddleware
+
     app.use("/auth", authRouter);
     app.use("/contacts", contactsRouter);
 
     app.use(notFoundHandler);
 
     app.use(errorHandler);
+
+    app.use('/uploads', express.static(UPLOAD_DIR));
 
     const port = Number(env("PORT", 3000));
 
